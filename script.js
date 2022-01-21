a095cd43f488fe3ff17603cc8d7c08aede3c6259
unknown <avrumiesafranovitz@gmail.com>
2022-01-21 00:42:36 -0500

    added generate password function

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // Assignment code here
-var passwordLength
 
+var upperCase
+var lowerCase
+var specialCharacter
+var choices
+var passwordLength
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+//creating generate password function
+var generatePassword = function() {
+    passwordLength = window.prompt("Input the character length of your password. Must be between 8 and 128 characters.");
+    if (passwordLength === null) {
+    window.alert("You must enter a password length.");}
+    //adding else statement
+    else if(passwordLength > 128 || passwordLength < 8) {
+      window.alert("You must choose a password between 8 and 128 characters.");
+    }
+}
 
 // Write password to the #password input
 function writePassword() {
